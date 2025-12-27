6bdba1933204529509bc96876a139a54041d357d
Subham0023 <subhamd931@gmail.com>
2025-12-27 18:24:46 +0530

    second commit

--- src/git_comments.jsx
+++ src/git_comments.jsx
@@ -1,2 +1,12 @@
-// git init -- Initializes a new Git repository in the current directory.
-// git remote -v -- Lists all configured remote repositories.
+// ! Git Commands Reference: - For first time setup of a Git repository ------------------------------------
+
+// * git init -- Initializes a new Git repository in the current directory.
+// ! git remote -v -- Lists all configured remote repositories.
+// ? git remote add origin <repository_url> -- Adds a new remote repository named 'origin'.
+// * git add . -- Stages all changes (new, modified, deleted files) for the next commit.
+// ? git commit -m "Your commit message" -- Commits the staged changes with a descriptive message.
+// ! git branch -- Lists all branches in the repository. The current branch is indicated with an asterisk (*).
+// ? git branch -m main -- Renames the current branch to 'main'.
+// * git push -u origin main -- Pushes the 'main' branch to the remote repository and sets it to track the remote branch.
+
+// ! Git Commands Reference: - For subsequent updates to the Git repository ------------------------------------
